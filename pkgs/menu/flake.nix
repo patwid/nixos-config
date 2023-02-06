e71560151310f435c2bb06bf85e779c69369a491
Patrick Widmer <patrick.widmer@tbwnet.ch>
2023-02-06 20:40:23 +0100

    Update pkgs

--- pkgs/menu/flake.nix
+++ pkgs/menu/flake.nix
@@ -1,34 +1,35 @@
 {
   description = "A menu";
 
-  outputs = { self, nixpkgs }:
-    let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in {
-      packages.x86_64-linux.default = self.packages.x86_64-linux.menu;
-      packages.x86_64-linux.menu = pkgs.stdenv.mkDerivation {
-        pname = "menu";
-        version = "1.0.0";
-        src = ./menu.sh;
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        packages.default = self.packages.menu;
+        packages.menu = pkgs.stdenv.mkDerivation {
+          pname = "menu";
+          version = "1.0.0";
+          src = ./menu.sh;
 
-        dontUnpack = true;
-        dontBuild = true;
-        dontConfigure = true;
+          dontUnpack = true;
+          dontBuild = true;
+          dontConfigure = true;
 
-        installPhase = with pkgs; ''
-          install -Dm 0755 $src $out/bin/menu
-          wrapProgram $out/bin/menu --set PATH \
-            "${
-              lib.makeBinPath [
-                bash
-                coreutils
-                foot
-                fzf
-              ]
-            }"
-        '';
+          installPhase = with pkgs; ''
+            install -Dm 0755 $src $out/bin/menu
+            wrapProgram $out/bin/menu --set PATH \
+              "${
+                lib.makeBinPath [
+                  bash
+                  coreutils
+                  foot
+                  fzf
+                ]
+              }"
+          '';
 
-        nativeBuildInputs = [ pkgs.makeWrapper ];
-      };
-    };
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+        };
+      });
 }
--- pkgs/menu_pass/flake.nix
+++ pkgs/menu_pass/flake.nix
@@ -1,34 +1,35 @@
 {
   description = "A passmenu";
 
-  outputs = { self, nixpkgs }:
-    let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in {
-      packages.x86_64-linux.default = self.packages.x86_64-linux.menu_pass;
-      packages.x86_64-linux.menu_pass = pkgs.stdenv.mkDerivation {
-        pname = "menu_pass";
-        version = "1.0.0";
-        src = ./menu_pass.sh;
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        packages.default = self.packages.menu_pass;
+        packages.menu_pass = pkgs.stdenv.mkDerivation {
+          pname = "menu_pass";
+          version = "1.0.0";
+          src = ./menu_pass.sh;
 
-        dontUnpack = true;
-        dontBuild = true;
-        dontConfigure = true;
+          dontUnpack = true;
+          dontBuild = true;
+          dontConfigure = true;
 
-        installPhase = with pkgs; ''
-          install -Dm 0755 $src $out/bin/menu_pass
-          wrapProgram $out/bin/menu_pass --set PATH \
-            "${
-              lib.makeBinPath [
-                bash
-                coreutils
-                findutils
-                gnused
-              ]
-            }"
-        '';
+          installPhase = with pkgs; ''
+            install -Dm 0755 $src $out/bin/menu_pass
+            wrapProgram $out/bin/menu_pass --set PATH \
+              "${
+                lib.makeBinPath [
+                  bash
+                  coreutils
+                  findutils
+                  gnused
+                ]
+              }"
+          '';
 
-        nativeBuildInputs = [ pkgs.makeWrapper ];
-      };
-    };
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+        };
+      });
 }
