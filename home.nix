4c25cca4577cc83d5a22cf7d1b8a9dd946732137
Patrick Widmer <patrick.widmer@tbwnet.ch>
2023-02-11 16:37:01 +0100

    Extract color definitions

--- home.nix
+++ home.nix
@@ -2,22 +2,7 @@
 
 let
   user = "patwid";
-  colors = {
-    black = "#181818";
-    darkestGrey = "#282828";
-    darkerGrey = "#383838";
-    darkGrey = "#585858";
-    lightGrey = "#b8b8b8";
-    lighterGrey = "#d8d8d8";
-    lightestGrey = "#e8e8e8";
-    white = "#f8f8f8";
-    red = "#ab4642";
-    green = "#a1b56c";
-    yellow = "#f7ca88";
-    blue = "#7cafc2";
-    magenta = "#ba8baf";
-    cyan = "#86c1b9";
-  };
+  colors = import ./colors { };
 in {
   imports = [
     home-manager.nixosModule
