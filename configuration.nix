94e56937fc74a2930189a343c0229c6f39f371b5
Patrick Widmer <patrick.widmer@tbwnet.ch>
2023-02-19 21:20:05 +0100

    nix: enable weekly auto upgrade

--- configuration.nix
+++ configuration.nix
@@ -32,6 +32,11 @@
 
   console.keyMap = "us";
 
+  system.autoUpgrade = {
+    enable = true;
+    dates = "weekly";
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
