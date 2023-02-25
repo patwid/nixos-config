2f15a6b39804804c8dd6a2a8468803565aed9da1
Patrick Widmer <patrick.widmer@tbwnet.ch>
2023-02-25 11:34:22 +0100

    aerc: add account config

--- aerc.nix
+++ aerc.nix
@@ -3,6 +3,25 @@ let
   user = import ./user.nix;
 in {
   home-manager.users.${user} = {
-    home.packages = [ pkgs.aerc ];
+    programs.aerc.enable = true;
+    programs.aerc.extraConfig = {
+      general.unsafe-accounts-conf = true;
+    };
+
+    accounts.email.accounts = {
+      Personal = {
+        primary = true;
+        realName = "Patrick Widmer";
+        address = "patrick.widmer@tbwnet.ch";
+        userName = "patrick.widmer@tbwnet.ch";
+        imap.host = "imap.tbwnet.ch";
+        imap.port = 993;
+        smtp.host = "smtp.tbwnet.ch";
+        smtp.port = 465;
+        passwordCommand = "pass mail/tbwnet | head -n 1";
+
+        aerc.enable = true;
+      };
+    };
   };
 }
