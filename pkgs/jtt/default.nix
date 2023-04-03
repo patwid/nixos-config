0f42e35177b89513ca196b980b21c411f802ae30
Patrick Widmer <patrick.widmer@tbwnet.ch>
2023-04-03 22:41:06 +0200

    jtt: do not build fat jar

--- pkgs/jtt/default.nix
+++ pkgs/jtt/default.nix
@@ -3,7 +3,8 @@
 let
   name = "jtt";
   version = "4.3.12-39";
-  src = "jtt-all-${version}-gcf4b0f5-dirty.jar";
+  src = "${name}-${version}-gcf4b0f5-dirty.tar";
+  jdk = jdk17.override { enableJavaFX = true; };
 in
 stdenv.mkDerivation {
   pname = "${name}";
@@ -11,30 +12,28 @@ stdenv.mkDerivation {
 
   src = requireFile {
     name = "${src}";
-    sha256 = "d4b37a5a099dbda557c7882804cfa091c7e09d249f0167ffeac366f4d3cbec45";
+    sha256 = "12lw7jdq4jgq7rsfwph659ibj01vr8wbp5xpi37gzvkffrsmwcg5";
 
     message = ''
       nix-prefetch-url file://$\PWD/${src}
     '';
   };
 
-  dontUnpack = true;
-
-  nativeBuildInputs = [ gradle jdk17 makeWrapper ];
+  nativeBuildInputs = [ gradle jdk makeWrapper ];
 
   buildPhase = ''
-    mkdir -p $out/share/java
-    cp $src $out/share/java/${name}.jar
+    mkdir -p $out/lib
+    cp -a lib/*.jar $out/lib
   '';
 
   installPhase = ''
     mkdir -p $out/bin
-    makeWrapper ${jdk17}/bin/java $out/bin/${name} \
+    makeWrapper ${jdk}/bin/java $out/bin/${name} \
       --add-flags "--add-exports=javafx.base/com.sun.javafx.event=ALL-UNNAMED" \
       --add-flags "--add-exports=javafx.controls/com.sun.javafx.scene.control=ALL-UNNAMED" \
       --add-flags "--add-exports=javafx.base/com.sun.javafx.collections=ALL-UNNAMED" \
       --add-flags "--add-opens=javafx.controls/javafx.scene.control.skin=ALL-UNNAMED" \
-      --add-flags "-jar $out/share/java/${name}.jar" \
+      --add-flags "-cp \"$out/lib/*\" ch.ergon.jtt.TableToolStarter" \
       --add-flags "--trace=3" \
       --add-flags "--no-update" \
       --add-flags "--enable-remotecontrol" \
