8ed09734c886b8af2cc7a1f20b9f369c1afb9a74
Patrick Widmer <patrick.widmer@ergon.ch>
2023-04-11 09:37:40 +0200

    pkgs: remove zulu17

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,7 +4,6 @@ let
   callPackage = lib.callPackageWith (pkgs // localPkgs);
   localPkgs = {
     xdg-open = callPackage ./xdg-open { };
-    zulu17 = callPackage ./zulu17 { };
     jtt = callPackage ./jtt { };
     menu = callPackage ./menu { };
     menu-pass = callPackage ./menu-pass { };
