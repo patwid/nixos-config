7a0848d44b6575714b73c9c185d1688f09343fbe
Patrick Widmer <patrick.widmer@tbnwet.ch>
2023-04-12 20:20:28 +0200

    users: add custom group and gid option

--- modules/users.nix
+++ modules/users.nix
@@ -1,12 +1,23 @@
-{ args, pkgs, ... }:
+{ config, args, lib, pkgs, ... }:
 let
   inherit (args) user;
+  customGroup = args ? group && args ? gid;
 in
 {
-  users.users.${user} = {
-    isNormalUser = true;
-    description = "${user}";
-    extraGroups = [ "wheel" ];
-    packages = with pkgs; [ ];
+  users = {
+    users.${user} = {
+      isNormalUser = true;
+      description = "${user}";
+      extraGroups = [ "wheel" ];
+      packages = with pkgs; [ ];
+    }
+    // lib.attrsets.optionalAttrs customGroup {
+      inherit (args) group;
+    };
+  }
+  // lib.attrsets.optionalAttrs customGroup {
+    groups.${args.group} = {
+      inherit (args) gid;
+    };
   };
 }
