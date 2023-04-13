140c6354226488b51058e0c51e529cc1180aabaf
Patrick Widmer <patrick.widmer@ergon.ch>
2023-04-13 09:06:51 +0200

    hardware: enable performance mode

--- hosts/cohen/hardware-configuration.nix
+++ hosts/cohen/hardware-configuration.nix
@@ -37,5 +37,6 @@
   # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
