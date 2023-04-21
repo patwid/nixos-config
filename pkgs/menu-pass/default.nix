cf38c6e5c61b0c0c64d8c55745fed7e48f40c0a5
Patrick Widmer <patrick.widmer@tbnwet.ch>
2023-04-21 20:41:00 +0200

    feat(menu-pass): strip leading slash

--- pkgs/menu-pass/default.nix
+++ pkgs/menu-pass/default.nix
@@ -13,7 +13,7 @@ writeShellApplication {
   text = ''
     prefix=''${PASSWORD_STORE_DIR:-~/.password-store}
     find "$prefix" -name '*.gpg' \
-            | sed "s,^$prefix\(.*\)\.gpg$,\1," \
+            | sed "s,^$prefix/\(.*\)\.gpg$,\1," \
             | sort \
             | wmenu \
             | xargs --no-run-if-empty pass show --clip
