e9c9a15ec66abc8972f546c6daac6a1dd2543332
Patrick Widmer <patrick.widmer@ergon.ch>
2023-11-04 11:31:50 +0100

    chore(apple-silicon): clean up

--- modules/+aarch64/apple-silicon.nix
+++ modules/+aarch64/apple-silicon.nix
@@ -10,7 +10,6 @@ in
   config = lib.mkIf (apple-silicon) {
     hardware.asahi.peripheralFirmwareDirectory = ../../firmware;
     hardware.asahi.useExperimentalGPUDriver = true;
-    # hardware.asahi.pkgsSystem = "x86_64-linux";
 
     nixpkgs.overlays = [
       nixos-apple-silicon.overlays.apple-silicon-overlay
