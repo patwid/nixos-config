5036c7b7d60fab3a09f48b514ece64268101bbdc
Patrick Widmer <patrick.widmer@ergon.ch>
2023-11-04 16:34:14 +0100

    feat(boot): increase number of entries

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,7 +1,7 @@
 { lib, ... }:
 {
   boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 5;
+  boot.loader.systemd-boot.configurationLimit = 10;
   boot.loader.efi.efiSysMountPoint = lib.mkDefault "/boot/efi";
   boot.loader.efi.canTouchEfiVariables = lib.mkDefault true;
 
