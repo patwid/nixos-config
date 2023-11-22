2cd25f3f37f51299164045641510c5b3cd966da7
Patrick Widmer <patrick.widmer@ergon.ch>
2023-11-22 17:48:30 +0100

    feat: switch to fzf menu

--- pkgs/menu-pass/default.nix
+++ pkgs/menu-pass/default.nix
@@ -2,20 +2,20 @@
 , coreutils
 , findutils
 , gnused
-, wmenu
+, menu
 , pass
 , wl-clipboard
 }:
 
 writeShellApplication {
   name = "menu-pass";
-  runtimeInputs = [ coreutils findutils gnused wmenu pass wl-clipboard ];
+  runtimeInputs = [ coreutils findutils gnused menu pass wl-clipboard ];
   text = ''
     prefix=''${PASSWORD_STORE_DIR:-~/.password-store}
     exec find "$prefix" -name '*.gpg' \
             | sed "s,^$prefix/\(.*\)\.gpg$,\1," \
             | sort \
-            | wmenu \
+            | menu \
             | xargs --no-run-if-empty pass show --clip
   '';
 }
