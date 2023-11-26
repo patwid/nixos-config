b9f13bef39f8265f212d6b90bbc4fae1b1e27e9c
Patrick Widmer <patrick.widmer@ergon.ch>
2023-11-26 10:41:25 +0100

    feat(menu-news): remove redundant flag

--- pkgs/menu-news/default.nix
+++ pkgs/menu-news/default.nix
@@ -5,7 +5,7 @@ writeShellApplication {
   runtimeInputs = [ coreutils sfeed wmenu ];
   text = ''
     url=''$(sfeed_plain "$HOME/.config/sfeed/feeds/"* | sort --reverse |
-        wmenu -l 35 -i | sed -n 's@^.* \([a-zA-Z]*://\)\(.*\)$@\1\2@p')
+        wmenu -l 35 | sed -n 's@^.* \([a-zA-Z]*://\)\(.*\)$@\1\2@p')
     test -n "''${url}" && exec xdg-open "''${url}"
   '';
 }
