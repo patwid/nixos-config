a309e53636898e99af627d6a801d010693d008fc
Patrick Widmer <patrick.widmer@ergon.ch>
2023-11-26 12:15:33 +0100

    feat(menu): update feeds

--- pkgs/menu-news/default.nix
+++ pkgs/menu-news/default.nix
@@ -4,6 +4,7 @@ writeShellApplication {
   name = "menu-news";
   runtimeInputs = [ coreutils sfeed wmenu ];
   text = ''
+    sfeed_update "$HOME/.config/sfeed/news/sfeedrc"
     url=''$(sfeed_plain "$HOME/.config/sfeed/news/feeds/"* | sort --reverse |
         wmenu -l 35 | sed -n 's@^.* \([a-zA-Z]*://\)\(.*\)$@\1\2@p')
     test -n "''${url}" && exec xdg-open "''${url}"
--- pkgs/menu-podcasts/default.nix
+++ pkgs/menu-podcasts/default.nix
@@ -4,6 +4,7 @@ writeShellApplication {
   name = "menu-podcasts";
   runtimeInputs = [ coreutils sfeed wmenu ];
   text = ''
+    sfeed_update "$HOME/.config/sfeed/podcasts/sfeedrc"
     url=''$(sfeed_plain "$HOME/.config/sfeed/podcasts/feeds/"* | sort --reverse |
         wmenu -l 35 | sed -n 's@^.* \([a-zA-Z]*://\)\(.*\)$@\1\2@p')
     test -n "''${url}" && exec mpv "''${url}"
