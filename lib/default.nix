7f0e39a7611a85d233cd927a64307c97fd4a0e3e
Patrick Widmer <patrick.widmer@ergon.ch>
2023-12-23 14:10:35 +0100

    feat: refactor lib

--- lib/default.nix
+++ lib/default.nix
@@ -1,9 +1,9 @@
 { lib }:
 let
   filesystem = import ./filesystem.nix { inherit lib; };
+  colors = import ./colors.nix { inherit lib; };
 in
 {
   inherit (filesystem) modulesIn;
-
-  colors = import ./colors.nix { inherit lib; };
+  inherit colors;
 }
