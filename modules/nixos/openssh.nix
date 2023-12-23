93235c66a4f2b15dce05f6130c48cdd394b9c35e
Patrick Widmer <patrick.widmer@ergon.ch>
2023-12-23 18:45:01 +0100

    feat: add authorized ssh keys

--- modules/nixos/openssh.nix
+++ modules/nixos/openssh.nix
@@ -1,3 +1,7 @@
+{ config, ... }:
+let
+  inherit (config) user;
+in
 {
   services.openssh = {
     enable = true;
@@ -5,5 +9,13 @@
       PasswordAuthentication = false;
       PermitRootLogin = "no";
     };
+  };
+
+  users.users.${user.name} = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIxXYugJIENGOXJIY11n2H+yHbfBLoh1pByszOe1s2BQ patwid@desktop"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKDwzQD/7hBZakOKm3Fxv4r8qz/y0MiDxuJ2X8hj8sJn patwid@htpc"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINpXEq+FIUxQkyX8yhm6jrXDJNZQn6H6nifNY5KsUZgh patwid@laptop"
+    ];
   };
 }
