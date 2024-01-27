d0a6e7d736c05bec042ec4fe7fa673f9603a9b12
Patrick Widmer <patrick.widmer@ergon.ch>
2024-01-27 17:26:05 +0100

    feat: configure hid_apple kernel module

--- modules/nixos/+aarch64/apple-silicon.nix
+++ modules/nixos/+aarch64/apple-silicon.nix
@@ -24,6 +24,12 @@ in
     boot.loader.efi.canTouchEfiVariables = false;
     boot.loader.efi.efiSysMountPoint = "/boot";
 
+    boot.extraModprobeConfig = ''
+      options hid_apple fnmode=2
+      options hid_apple swap_opt_cmd=1
+      options hid_apple swap_fn_leftctrl=1
+    '';
+
     system.autoUpgrade.flags = [ "--impure" ];
   };
 }
