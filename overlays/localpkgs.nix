836913eb81e29934e2cb572185e9c00d857c6d18
Patrick Widmer <patrick.widmer@ergon.ch>
2024-02-08 20:39:57 +0100

    feat: refactor localpkgs

--- overlays/localpkgs.nix
+++ overlays/localpkgs.nix
@@ -1,26 +1,6 @@
 { ... }@attrs:
 let
-  localpkgs = import ../pkgs attrs;
+  inherit (import ../pkgs attrs) pkgs vimPlugins;
 in
-self: super: {
-  inherit (localpkgs)
-    xdg-open
-    jtt
-    google-chrome
-    notify-low-battery
-    mattermost
-    menu-run
-    menu-pass
-    menu-news
-    menu-videos
-    menu-podcasts
-    outlook
-    smartaz
-    teams
-    whatsapp
-    hiprompt-gtk-py;
-
-  vimPlugins = super.vimPlugins.extend (_: _: {
-    inherit (localpkgs) vim-paige vim-simple;
-  });
-}
+self: super:
+pkgs // { vimPlugins = super.vimPlugins.extend (_: _: vimPlugins); }
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,28 +3,48 @@
 let
   callPackage = lib.callPackageWith (pkgs // localPkgs);
   localPkgs = {
-    xdg-open = callPackage ./xdg-open { };
-    jtt = callPackage ./jtt { };
-    menu = callPackage ./menu { };
-    menu-pass = callPackage ./menu-pass { };
-    menu-run = callPackage ./menu-run { };
-    menu-news = callPackage ./menu-feed { name = "news"; opener = "xdg-open"; };
-    menu-videos = callPackage ./menu-feed { name = "videos"; opener = "mpv"; };
-    menu-podcasts = callPackage ./menu-feed { name = "podcasts"; opener = "mpv"; };
     google-chrome = callPackage ./google-chrome { };
-    outlook = callPackage ./webapp { name = "outlook"; url = "https://outlook.office.com/mail/"; };
-    mattermost = callPackage ./webapp { name = "mattermost"; url = "https://mattermost.ergon.ch/"; };
-    teams = callPackage ./webapp { name = "teams"; url = "https://teams.microsoft.com/"; };
-    smartaz = callPackage ./webapp { name = "smartaz"; url = "https://smartaz.ergon.ch/"; };
-    whatsapp = callPackage ./webapp { name = "whatsapp"; url = "https://web.whatsapp.com/"; };
-    notify-low-battery = callPackage ./notify {
-      name = "notify-low-battery";
-      summary = "Warning";
-      body = "Low Battery";
-    };
     hiprompt-gtk-py = callPackage ./hiprompt-gtk-py { };
+    jtt = callPackage ./jtt { };
+    mattermost = callPackage ./webapp { name = "mattermost"; url = "https://mattermost.ergon.ch/"; };
+    menu = callPackage ./menu { };
+    menu-news = callPackage ./menu-feed { name = "news"; opener = "xdg-open"; };
+    menu-pass = callPackage ./menu-pass { };
+    menu-podcasts = callPackage ./menu-feed { name = "podcasts"; opener = "mpv"; };
+    menu-run = callPackage ./menu-run { };
+    menu-videos = callPackage ./menu-feed { name = "videos"; opener = "mpv"; };
+    notify-low-battery = callPackage ./notify { name = "notify-low-battery"; summary = "Warning"; body = "Low Battery"; };
+    outlook = callPackage ./webapp { name = "outlook"; url = "https://outlook.office.com/mail/"; };
+    smartaz = callPackage ./webapp { name = "smartaz"; url = "https://smartaz.ergon.ch/"; };
+    teams = callPackage ./webapp { name = "teams"; url = "https://teams.microsoft.com/"; };
+    whatsapp = callPackage ./webapp { name = "whatsapp"; url = "https://web.whatsapp.com/"; };
+    xdg-open = callPackage ./xdg-open { };
+
     vim-paige = callPackage ./vim-paige { };
     vim-simple = callPackage ./vim-simple { };
   };
 in
-localPkgs
+{
+  pkgs = {
+    inherit (localPkgs)
+      google-chrome
+      hiprompt-gtk-py
+      jtt
+      mattermost
+      menu-news
+      menu-pass
+      menu-podcasts
+      menu-run
+      menu-videos
+      notify-low-battery
+      outlook
+      smartaz
+      teams
+      whatsapp
+      xdg-open;
+  };
+
+  vimPlugins = {
+    inherit (localPkgs) vim-paige vim-simple;
+  };
+}
