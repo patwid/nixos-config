928834fccecec53ae6ddde7c0ece216ea08f59df
Patrick Widmer <patrick.widmer@ergon.ch>
2024-03-19 19:36:29 +0100

    feat(idea): add custom vmopts

--- modules/home-manager/idea.nix
+++ modules/home-manager/idea.nix
@@ -1,11 +1,19 @@
 { nixosConfig, lib, pkgs, ... }:
 let
   inherit (nixosConfig) work;
+  inherit (pkgs) jetbrains;
+
+  idea-ultimate = jetbrains.idea-ultimate.override {
+    vmopts = ''
+      -Xms4g
+      -Xmx4g
+    '';
+  };
+
+  idea-ultimate' = jetbrains.plugins.addPlugins idea-ultimate [ "ideavim" ];
 in
 lib.mkIf (work.enable) {
-  home.packages = with pkgs; [
-    (jetbrains.plugins.addPlugins jetbrains.idea-ultimate [ "ideavim" ])
-  ];
+  home.packages = [ idea-ultimate' ];
 
   xdg.configFile."ideavim/ideavimrc".text = ''
     set clipboard+=unnamedplus,ideaput
