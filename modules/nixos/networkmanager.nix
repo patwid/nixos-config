79b5ece6ea76fff3ffb073fd038a51f1c48d1272
Patrick Widmer <patrick.widmer@ergon.ch>
2024-04-13 08:29:12 +0200

    feat(work): update firewall rules

--- modules/nixos/networkmanager.nix
+++ modules/nixos/networkmanager.nix
@@ -5,5 +5,11 @@ in
 {
   networking.networkmanager.enable = true;
 
+  # Allow incoming traffic from axenita-onlyoffice.ergon.ch (nslookup <domain>)
+  networking.firewall.extraCommands = ''
+    iptables -I INPUT -s 87.239.210.194 -j ACCEPT
+    iptables -I INPUT -s 192.169.0.0/24 -j ACCEPT
+  '';
+
   users.users.${user.name}.extraGroups = [ "networkmanager" ];
 }
