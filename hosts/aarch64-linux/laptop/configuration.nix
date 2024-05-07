75cb311376eed9837638ab9e9541b9366b0aaf25
Patrick Widmer <patrick.widmer@ergon.ch>
2024-05-07 21:09:43 +0200

    feat: always use tmpfs

--- hosts/aarch64-linux/laptop/configuration.nix
+++ hosts/aarch64-linux/laptop/configuration.nix
@@ -14,8 +14,6 @@
     eDP-1 = "1.5";
   };
 
-  boot.tmp.useTmpfs = false;
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -1,13 +1,9 @@
-{ config, lib, ... }:
-let
-  inherit (config.boot.tmp) useTmpfs;
-in
+{ lib, ... }:
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 10;
   boot.loader.efi.efiSysMountPoint = lib.mkDefault "/boot/efi";
   boot.loader.efi.canTouchEfiVariables = lib.mkDefault true;
 
-  boot.tmp.useTmpfs = lib.mkDefault true;
-  boot.tmp.cleanOnBoot = !useTmpfs;
+  boot.tmp.useTmpfs = true;
 }
