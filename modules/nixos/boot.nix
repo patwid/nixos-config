7c48dd430ba5c91459fa43ae70904a94de955155
Patrick Widmer <patrick.widmer@ergon.ch>
2024-05-09 10:25:29 +0200

    Revert "feat: always use tmpfs"
    This reverts commit 75cb311376eed9837638ab9e9541b9366b0aaf25.

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -1,9 +1,13 @@
-{ lib, ... }:
+{ config, lib, ... }:
+let
+  inherit (config.boot.tmp) useTmpfs;
+in
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 10;
   boot.loader.efi.efiSysMountPoint = lib.mkDefault "/boot/efi";
   boot.loader.efi.canTouchEfiVariables = lib.mkDefault true;
 
-  boot.tmp.useTmpfs = true;
+  boot.tmp.useTmpfs = lib.mkDefault true;
+  boot.tmp.cleanOnBoot = !useTmpfs;
 }
