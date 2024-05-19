39ddd15eb41dc1806f2ce717082de3887065ba91
Patrick Widmer <patrick.widmer@ergon.ch>
2024-05-19 15:07:11 +0200

    chore: only import nix modules

--- lib/filesystem.nix
+++ lib/filesystem.nix
@@ -1,13 +1,13 @@
 { lib }:
 let
   inherit (builtins) toString;
-  inherit (lib) filter hasInfix head splitString;
+  inherit (lib) filter hasInfix hasSuffix head splitString;
   inherit (lib.filesystem) listFilesRecursive;
 
   archOf = system: head (splitString "-" system);
   isOptional = path: hasInfix "+" path;
   isArch = { path, arch }: hasInfix "+${arch}" path;
-  isImportable = { path, arch }@args: !isOptional path || isArch args;
+  isImportable = { path, arch }@args: hasSuffix ".nix" path && (!isOptional path || isArch args);
 in
 {
   modulesIn = system: path: filter (p: isImportable { arch = archOf system; path = (toString p); })
