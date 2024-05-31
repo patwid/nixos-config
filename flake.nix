1106bbfaa749dc715ddb5934d91a802c3bcee06a
Patrick Widmer <patrick.widmer@ergon.ch>
2024-06-01 00:57:26 +0200

    chore: update flake and stable pkgs to nixos 24.05

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware";
 
