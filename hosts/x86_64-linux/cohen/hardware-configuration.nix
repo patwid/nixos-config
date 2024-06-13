9b1009be1a25f61cdb7b57eeefee45c0dfd0860c
Patrick Widmer <patrick.widmer@ergon.ch>
2024-06-13 12:35:51 +0200

    feat(host): update hw config for cohen

--- hosts/x86_64-linux/cohen/hardware-configuration.nix
+++ hosts/x86_64-linux/cohen/hardware-configuration.nix
@@ -5,38 +5,36 @@
 
 {
   imports =
-    [
-      (modulesPath + "/installer/scan/not-detected.nix")
+    [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    {
-      device = "/dev/disk/by-partlabel/nixos";
+    { device = "/dev/disk/by-partlabel/nixos";
       fsType = "ext4";
     };
 
   fileSystems."/boot/efi" =
-    {
-      device = "/dev/disk/by-partlabel/esp";
+    { device = "/dev/disk/by-partlabel/esp";
       fsType = "vfat";
+      options = [ "fmask=0777" "dmask=0777" ];
     };
 
   swapDevices =
-    [{ device = "/dev/disk/by-partlabel/swap"; }];
+    [ { device = "/dev/disk/by-partlabel/swap"; }
+    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
+  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
