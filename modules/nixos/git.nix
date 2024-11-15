0c5265b5872503fffe53c525a479409d2050a796
Patrick Widmer <patrick.widmer@ergon.ch>
2024-11-15 08:24:39 +0100

    feat(git): add safe directory config

--- modules/nixos/git.nix
+++ modules/nixos/git.nix
@@ -1,4 +1,13 @@
-{ ... }:
+{ config, ... }:
+let
+  inherit (config) user;
+in
 {
-  programs.git.enable = true;
+  programs.git = {
+    enable = true;
+
+    config = {
+      safe.directory = "/home/${user.name}/.config/nixos";
+    };
+  };
 }
