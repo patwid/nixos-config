9c60f8c85d0a5ed2c0843336c0002b6f388a38a3
Patrick Widmer <patrick.widmer@ergon.ch>
2024-12-09 13:21:34 +0100

    feat: refactor dwl overlay

--- overlays/dwl/default.nix
+++ overlays/dwl/default.nix
@@ -1,25 +1,31 @@
 { lib, pkgs, ... }:
 final: prev:
 let
-  dwl = prev.dwl.overrideAttrs (old: {
-    patches = (old.patches or [ ]) ++ [
+  swallow = {
+    patches = [
       (prev.fetchpatch {
         url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/swallow/swallow.patch";
         hash = "sha256-1Vdt0FgbV7jHjr4wqWqDnaqVk5FcRnlYKb8/U0VUesM=";
       })
+    ];
+  };
 
+  bar = {
+    patches = [
       (prev.fetchpatch {
         url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/bar/bar-0.7.patch";
         hash = "sha256-WbT3wp3hIp6ixud9q27F+hn7zq/4be6ZUpGWtCWX9tw=";
       })
     ];
 
-    buildInputs = (old.buildInputs or [ ]) ++ (with prev; [
-      libdrm # bar patch
-      fcft # bar patch
-    ]);
-  });
+    buildInputs = with prev; [
+      libdrm
+      fcft
+    ];
+  };
 in
 {
-  dwl = dwl.override { configH = ./config.h; };
+  dwl = prev.dwl
+    .overrideAttrs (old: lib.mkMerge [ swallow bar ])
+    .override { configH = ./config.h; };
 }
