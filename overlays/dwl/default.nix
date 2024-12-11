bd7aa3879b7c70f0300514820a13ac74995d00fd
Patrick Widmer <patrick.widmer@ergon.ch>
2024-12-11 08:51:51 +0100

    chore: refactor dwl overlay

--- overlays/dwl/default.nix
+++ overlays/dwl/default.nix
@@ -1,31 +1,29 @@
 { lib, pkgs, ... }:
-final: prev:
-let
-  swallow = {
-    patches = [
-      (prev.fetchpatch {
-        url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/swallow/swallow.patch";
-        hash = "sha256-1Vdt0FgbV7jHjr4wqWqDnaqVk5FcRnlYKb8/U0VUesM=";
-      })
-    ];
-  };
-
-  bar = {
-    patches = [
-      (prev.fetchpatch {
-        url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/bar/bar-0.7.patch";
-        hash = "sha256-WbT3wp3hIp6ixud9q27F+hn7zq/4be6ZUpGWtCWX9tw=";
-      })
-    ];
-
-    buildInputs = with prev; [
-      libdrm
-      fcft
-    ];
-  };
-in
-{
+final: prev: {
   dwl = prev.dwl
-    .overrideAttrs (old: lib.mkMerge [ swallow bar ])
-    .override { configH = ./config.h; };
+    .override { configH = ./config.h; }
+    .overrideAttrs (old: lib.mkMerge [
+      {
+        patches = [
+          (prev.fetchpatch {
+            url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/swallow/swallow.patch";
+            hash = "sha256-1Vdt0FgbV7jHjr4wqWqDnaqVk5FcRnlYKb8/U0VUesM=";
+          })
+        ];
+      }
+
+      {
+        patches = [
+          (prev.fetchpatch {
+            url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/bar/bar-0.7.patch";
+            hash = "sha256-WbT3wp3hIp6ixud9q27F+hn7zq/4be6ZUpGWtCWX9tw=";
+          })
+        ];
+
+        buildInputs = with prev; [
+          libdrm
+          fcft
+        ];
+      }
+    ]);
 }
