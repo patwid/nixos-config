7a482b6365467cd635e99042877c5cff3e4e9c5d
Patrick Widmer <patrick.widmer@ergon.ch>
2025-01-26 11:08:41 +0100

    feat: dynamically call localpkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,19 +2,9 @@
 
 let
   callPackage = lib.callPackageWith (pkgs // localPkgs);
-  localPkgs = {
-    google-chrome = callPackage ./google-chrome { };
-    hiprompt-gtk-py = callPackage ./hiprompt-gtk-py { };
-    jtt = callPackage ./jtt { };
-    menu = callPackage ./menu { };
-    menu-feed = callPackage ./menu-feed { };
-    menu-mpv = callPackage ./menu-mpv { };
-    menu-pass = callPackage ./menu-pass { };
-    menu-run = callPackage ./menu-run { };
-    notify = callPackage ./notify { };
-    webapp = callPackage ./webapp { };
-    xdg-open = callPackage ./xdg-open { };
-    ytm = callPackage ./ytm { };
-  };
+  localPkgs =
+    builtins.readDir ./.
+    |> lib.filterAttrs (n: _: n != "default.nix")
+    |> builtins.mapAttrs (p: _: callPackage ./${p} { });
 in
 localPkgs
