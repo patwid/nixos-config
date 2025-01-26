72aabf240592d36c9aa672b6d1ea02274a01c969
Patrick Widmer <patrick.widmer@ergon.ch>
2025-01-26 13:19:16 +0100

    fix: idea overlay

--- modules/home-manager/idea.nix
+++ modules/home-manager/idea.nix
@@ -1,9 +1,15 @@
 { osConfig, lib, pkgs, ... }:
 let
-  inherit (osConfig) work;
+  inherit (osConfig) work ideaExtraVmopts;
 in
 lib.mkIf (work.enable) {
-  home.packages = with pkgs; [ jetbrains.idea-ultimate ];
+  home.packages = with pkgs; [
+    (jetbrains.plugins.addPlugins (jetbrains.idea-ultimate.override {
+      vmopts = ''
+        -Dawt.toolkit.name=WLToolkit
+      '' + ideaExtraVmopts;
+    }) [ "ideavim" ])
+  ];
 
   xdg.configFile."ideavim/ideavimrc".text = ''
     set clipboard+=unnamedplus,ideaput
