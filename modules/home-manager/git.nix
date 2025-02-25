880cbe7dae03f0326c074efc136bf0aab5e1832c
Patrick Widmer <patrick.widmer@ergon.ch>
2025-02-25 16:39:46 +0100

    feat: update git config

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -10,10 +10,53 @@ in
 
     lfs.enable = true;
     extraConfig = {
+      column.ui = "auto";
+      branch.sort = "-committeredate";
+      tag.sort = "version:refname";
       init.defaultBranch = "master";
+
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
+
+      push = {
+        default = "simple";
+        autoSetupRemote = true;
+        followTags = true;
+      };
+
+      fetch = {
+        all = true;
+        prune = true;
+        pruneTags = true;
+      };
+
+      help.autocorrect = "prompt";
+      commit.verbose = true;
+
+      # Reuse recorded resolutions
+      rerere = {
+        enabled = true;
+        autoupdate = true;
+      };
+
       core.editor = "nvim";
-      rebase.autoStash = true;
-      rerere.enabled = true;
+      # core.excludesfile = "~/.gitignore"; # global gitignore
+
+      rebase = {
+        autoSquash = true;
+        autoStash = true;
+        updateRefs = true;
+      };
+
+      # merge.conflictstyle = zdiff3;
+
+      # Resources:
+      # - https://jvns.ca/blog/2024/02/16/popular-git-config-options/
+      # - https://blog.gitbutler.com/how-git-core-devs-configure-git/
     };
   };
 }
