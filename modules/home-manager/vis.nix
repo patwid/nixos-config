a98d3ce93c63192cd02b8cecbd0a43b08a386002
Patrick Widmer <patrick.widmer@ergon.ch>
2025-03-16 18:11:50 +0100

    feat: add vis config files

--- modules/home-manager/vis.nix
+++ modules/home-manager/vis.nix
@@ -8,4 +8,14 @@
   home.packages = builtins.attrValues {
     inherit (pkgs) vis;
   };
+
+  programs.bash.shellAliases = {
+    "vi" = "vis";
+    "vim" = "vis";
+  };
+
+  xdg.configFile."vis" = {
+    source = ./vis;
+    recursive = true;
+  };
 }
