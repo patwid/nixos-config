2065eef81d28bee0745ca328e110cd135d87a1a0
Patrick Widmer <patrick.widmer@ergon.ch>
2025-08-03 19:29:37 +0200

    Revert "refactor: overlays use pipe"
    This reverts commit bd78aca6f69373fa3e528ac5fc13fde1c64361d8.

--- templates/java/nix/overlays/default.nix
+++ templates/java/nix/overlays/default.nix
@@ -1,11 +1,13 @@
 { lib }:
 final: prev:
-builtins.readDir ../packages
-|> lib.mapAttrs' (
-  name: _:
-  lib.nameValuePair (lib.removePrefix ".nix" name) (final.callPackage ../packages/${name} { })
-)
-|> lib.mergeAttrs {
+{
   jdk = prev.jdk21;
   gradle = prev.gradle.override { java = final.jdk; };
 }
+// (
+  builtins.readDir ../packages
+  |> lib.mapAttrs' (
+    name: _:
+    lib.nameValuePair (lib.removePrefix ".nix" name) (final.callPackage ../packages/${name} { })
+  )
+)
