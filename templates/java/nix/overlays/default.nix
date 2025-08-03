8c55760b3f78d6b4e50bdec7e0ffb37ec7505e6d
Patrick Widmer <patrick.widmer@ergon.ch>
2025-08-03 19:29:55 +0200

    refactor: template overlay

--- templates/java/nix/overlays/default.nix
+++ templates/java/nix/overlays/default.nix
@@ -1,13 +1,15 @@
 { lib }:
 final: prev:
+let
+  localpkgs =
+    builtins.readDir ../packages
+    |> lib.mapAttrs' (
+      name: _:
+      lib.nameValuePair (lib.removePrefix ".nix" name) (final.callPackage ../packages/${name} { })
+    );
+in
 {
   jdk = prev.jdk21;
   gradle = prev.gradle.override { java = final.jdk; };
 }
-// (
-  builtins.readDir ../packages
-  |> lib.mapAttrs' (
-    name: _:
-    lib.nameValuePair (lib.removePrefix ".nix" name) (final.callPackage ../packages/${name} { })
-  )
-)
+// localpkgs
