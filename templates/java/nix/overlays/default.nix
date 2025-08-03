bd78aca6f69373fa3e528ac5fc13fde1c64361d8
Patrick Widmer <patrick.widmer@ergon.ch>
2025-08-03 11:58:17 +0200

    refactor: overlays use pipe

--- templates/java/nix/overlays/default.nix
+++ templates/java/nix/overlays/default.nix
@@ -1,13 +1,11 @@
 { lib }:
 final: prev:
-{
+builtins.readDir ../packages
+|> lib.mapAttrs' (
+  name: _:
+  lib.nameValuePair (lib.removePrefix ".nix" name) (final.callPackage ../packages/${name} { })
+)
+|> lib.mergeAttrs {
   jdk = prev.jdk21;
   gradle = prev.gradle.override { java = final.jdk; };
 }
-// (
-  builtins.readDir ../packages
-  |> lib.mapAttrs' (
-    name: _:
-    lib.nameValuePair (lib.removePrefix ".nix" name) (final.callPackage ../packages/${name} { })
-  )
-)
