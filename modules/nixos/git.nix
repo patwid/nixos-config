19565a2c996aa1413edea7a105c580c8afb3567b
Patrick Widmer <patrick.widmer@ergon.ch>
2025-12-27 18:07:16 +0100

    Revert "feat(git): switch to wrapper module"
    This reverts commit d70f8d7cd92418e3151fac224011b61c07387a16.

--- modules/nixos/git.nix
+++ modules/nixos/git.nix
@@ -1,8 +1,63 @@
-{ pkgs, ... }:
+{ config, ... }:
+let
+  inherit (config) work;
+in
 {
   programs.git = {
     enable = true;
     lfs.enable = true;
-    package = pkgs.gitWrapped;
+
+    config = {
+      user.name = "Patrick Widmer";
+      user.email = if work.enable then "patrick.widmer@ergon.ch" else "patrick.widmer@tbwnet.ch";
+
+      column.ui = "auto";
+      branch.sort = "-committerdate";
+      tag.sort = "version:refname";
+      init.defaultBranch = "master";
+
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
+
+      push = {
+        default = "simple";
+        autoSetupRemote = true;
+        followTags = true;
+      };
+
+      fetch = {
+        all = true;
+        prune = true;
+        pruneTags = true;
+      };
+
+      help.autocorrect = "prompt";
+      commit.verbose = true;
+
+      # Reuse recorded resolutions
+      rerere = {
+        enabled = true;
+        autoupdate = true;
+      };
+
+      core.editor = "nvim";
+      # core.excludesfile = "~/.gitignore"; # global gitignore
+
+      rebase = {
+        autoSquash = true;
+        autoStash = true;
+        updateRefs = true;
+      };
+
+      # merge.conflictstyle = zdiff3;
+
+      # Resources:
+      # - https://jvns.ca/blog/2024/02/16/popular-git-config-options/
+      # - https://blog.gitbutler.com/how-git-core-devs-configure-git/
+    };
   };
 }
