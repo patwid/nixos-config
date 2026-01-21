f23d9fed204c8173298878ed0046003d70b48d30
Patrick Widmer <patrick.widmer@ergon.ch>
2026-01-21 22:05:08 +0100

    chore(neovim): drop custom inspect function

--- modules/wrappers/neovim/init.lua
+++ modules/wrappers/neovim/init.lua
@@ -16,14 +16,3 @@ vim.api.nvim_create_autocmd('ColorScheme', {
 
 vim.opt.background = 'light'
 vim.cmd('colorscheme simple')
-
--- Custom command to inspect syntax stack at current line and column
--- https://github.com/vim/colorschemes/wiki/How-to-override-a-colorscheme%3F
-vim.api.nvim_create_user_command('Inspect', function()
-  local syntax_stack = vim.fn.synstack(vim.fn.line('.'), vim.fn.col('.'))
-  local syntax_names = {}
-  for _, syn_id in ipairs(syntax_stack) do
-    table.insert(syntax_names, vim.fn.synIDattr(syn_id, 'name'))
-  end
-  print(vim.inspect(syntax_names))
-end, {})
