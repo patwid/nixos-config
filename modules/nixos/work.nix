ceb8784805525269b43f802ec37ff56cf6972a00
Patrick Widmer <patrick.widmer@ergon.ch>
2026-01-22 20:16:00 +0100

    chore: migrate hm to nixos

--- modules/nixos/work.nix
+++ modules/nixos/work.nix
@@ -1,7 +1,27 @@
-{ lib, pkgs, ... }:
+{ config, lib, pkgs, ... }:
+let
+  inherit (config) work;
+in
 {
   options.work = {
     enable = lib.mkEnableOption { };
     remote = lib.mkEnableOption { };
+  };
+
+  config =
+    lib.mkIf (work.enable) {
+      environment.systemPackages = builtins.attrValues {
+        inherit (pkgs)
+          _1password
+          mattermost
+          medbase
+          outlook
+          rds
+          sanacare
+          smartaz
+          teams
+          teleport_16
+          ;
+    };
   };
 }
