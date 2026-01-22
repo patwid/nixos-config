dc46c15fbee0742b7aa18eaf1e79ade88f0a9ad0
Patrick Widmer <patrick.widmer@ergon.ch>
2026-01-22 20:16:00 +0100

    chore: migrate hm to nixos

--- modules/nixos/packages.nix
+++ modules/nixos/packages.nix
@@ -1,4 +1,31 @@
-{ ... }:
+{ pkgs, ... }:
 {
   environment.defaultPackages = [ ];
+
+  environment.systemPackages = builtins.attrValues {
+    inherit (pkgs)
+      adwaita-icon-theme
+      curl
+      grim
+      imagemagick
+      imv
+      jq
+      libreoffice-fresh
+      man-pages
+      man-pages-posix
+      menu-pass
+      menu-run
+      pavucontrol
+      ripgrep
+      slurp
+      swaybg
+      telegram-web
+      unzip
+      wget
+      whatsapp
+      wl-clipboard
+      xdg-open
+      zip
+      ;
+  };
 }
