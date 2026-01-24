1489c47ca9cda4210ad60859bc91f93487b58656
Patrick Widmer <patrick.widmer@ergon.ch>
2026-01-24 16:37:33 +0100

    chore: drop vim colorscheme vim-simple

--- packages/default.nix
+++ packages/default.nix
@@ -4,20 +4,13 @@ let
   callPackage = lib.callPackageWith (pkgs // localPkgs);
 
   localPkgs =
-    (
-      builtins.readDir ./.
-      |> lib.filterAttrs (
-        n: _:
-        !builtins.elem n [
-          "default.nix"
-          "vim-plugins"
-        ]
-      )
-      |> builtins.mapAttrs (p: _: callPackage ./${p} { })
+    builtins.readDir ./.
+    |> lib.filterAttrs (
+      n: _:
+      !builtins.elem n [
+        "default.nix"
+      ]
     )
-    // {
-      vimPlugins =
-        builtins.readDir ./vim-plugins |> builtins.mapAttrs (p: _: callPackage ./vim-plugins/${p} { });
-    };
+    |> builtins.mapAttrs (p: _: callPackage ./${p} { });
 in
 localPkgs
