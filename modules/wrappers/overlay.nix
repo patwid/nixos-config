aaa946985ed5266cf33c593856f1ad75c20128aa
Patrick Widmer <patrick.widmer@ergon.ch>
2026-01-28 21:42:41 +0100

    chore: refactor wrapper overlay

--- modules/wrappers/overlay.nix
+++ modules/wrappers/overlay.nix
@@ -3,22 +3,21 @@
   config,
   lib,
 }:
-final: prev:
 builtins.readDir ../wrappers
-|> lib.filterAttrs (name: _: name != "overlay.nix")
-|> lib.mapAttrs' (
-  name: _:
-  lib.nameValuePair (lib.removeSuffix ".nix" name) (
-    import ../wrappers/${name} {
+|> builtins.attrNames
+|> lib.filter (name: name != "overlay.nix")
+|> map (
+  name:
+  let
+    pname = lib.removeSuffix ".nix" name;
+    # Prevent infinite recursion
+    pname' = if pname == "git" then "${pname}'" else pname;
+  in
+  final: prev: {
+    ${pname'} = import ../wrappers/${name} {
       inherit inputs config lib;
       pkgs = prev;
-    }
-  )
+    };
+  }
 )
-|> lib.mapAttrs' (
-  name: value:
-  lib.nameValuePair
-    # Prevent infinite recursion
-    (if name == "git" then "${name}'" else name)
-    value
-)
+|> lib.composeManyExtensions
