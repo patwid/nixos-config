6c974abdce4cfad3ce7e285b5bb583de4b014e24
Patrick Widmer <patrick.widmer@ergon.ch>
2026-03-21 18:07:03 +0100

    feat: enable apple silicon binary cache

--- modules/nixos/+aarch64/apple-silicon.nix
+++ modules/nixos/+aarch64/apple-silicon.nix
@@ -34,5 +34,14 @@ in
     '';
 
     boot.m1n1CustomLogo = ./empty.png;
+
+    nix.settings = {
+      extra-substituters = [
+        "https://nixos-apple-silicon.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
+      ];
+    };
   };
 }
