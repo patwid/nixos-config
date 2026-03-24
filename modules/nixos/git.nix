09a586502f9c2f8c6dec4c6ee99450175e348fa7
Patrick Widmer <patrick.widmer@tbwnet.ch>
2026-03-24 10:51:32 +0100

    refactor: merge git wrapper into git.nix
    Co-locate wrapper config into the NixOS module. Eliminates the git'
    overlay hack — wrappedGit is now a local let binding.

--- modules/nixos/git.nix
+++ modules/nixos/git.nix
@@ -1,8 +1,72 @@
-{ pkgs, ... }:
+{
+  inputs,
+  config,
+  pkgs,
+  ...
+}:
+let
+  inherit (inputs.nix-wrapper-modules.wrappers) git;
+  inherit (config) work;
+  inherit (config.environment.sessionVariables) EDITOR;
+
+  wrappedGit = git.wrap {
+    inherit pkgs;
+
+    settings = {
+      user.name = "Patrick Widmer";
+      user.email = if work.enable then "patrick.widmer@ergon.ch" else "patrick.widmer@tbwnet.ch";
+
+      column.ui = "auto";
+      branch.sort = "-committerdate";
+      tag.sort = "version:refname";
+      init.defaultBranch = "master";
+
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
+
+      push = {
+        default = "simple";
+        autoSetupRemote = true;
+        followTags = true;
+      };
+
+      fetch = {
+        all = true;
+        prune = true;
+        pruneTags = true;
+      };
+
+      help.autocorrect = "prompt";
+      commit.verbose = true;
+
+      # Reuse recorded resolutions
+      rerere = {
+        enabled = true;
+        autoupdate = true;
+      };
+
+      core.editor = EDITOR;
+
+      rebase = {
+        autoSquash = true;
+        autoStash = true;
+        updateRefs = true;
+      };
+
+      # Resources:
+      # - https://jvns.ca/blog/2024/02/16/popular-git-config-options/
+      # - https://blog.gitbutler.com/how-git-core-devs-configure-git/
+    };
+  };
+in
 {
   programs.git = {
     enable = true;
     lfs.enable = true;
-    package = pkgs.git';
+    package = wrappedGit;
   };
 }
