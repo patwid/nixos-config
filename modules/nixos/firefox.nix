2ed7db815304bfa21c02cf82019708d254e47826
Patrick Widmer <patrick.widmer@tbwnet.ch>
2026-03-26 18:15:31 +0000

    refactor: replace home-manager with NixOS-native programs.firefox

--- modules/nixos/firefox.nix
+++ modules/nixos/firefox.nix
@@ -1,77 +1,79 @@
 {
-  config,
   pkgs,
   ...
 }:
-let
-  inherit (config) user;
-in
 {
-  home-manager.users.${user.name} = {
-    programs.firefox = {
-      enable = true;
-      package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
-        # https://mozilla.github.io/policy-templates/
-        extraPolicies = {
-          DisableFirefoxStudies = true;
-          DisablePocket = true;
-          DisableTelemetry = true;
-          DisableFirefoxAccounts = true;
-          NoDefaultBookmarks = true;
-          OfferToSaveLogins = false;
-          OffertosaveloginsDefault = false;
-          PasswordManagerEnabled = false;
+  programs.firefox = {
+    enable = true;
+
+    # https://mozilla.github.io/policy-templates/
+    policies = {
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DisableFirefoxAccounts = true;
+      NoDefaultBookmarks = true;
+      OfferToSaveLogins = false;
+      OffertosaveloginsDefault = false;
+      PasswordManagerEnabled = false;
+
+      # Install extensions via policy
+      ExtensionSettings = {
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
         };
       };
-      profiles.default = {
-        extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
-          ublock-origin
+
+      # Set default search engine
+      SearchEngines = {
+        Default = "DuckDuckGo";
+        Remove = [
+          "Amazon.de"
+          "Bing"
+          "eBay"
+          "Google"
+          "Wikipedia"
         ];
-        search.default = "ddg";
-        search.force = true;
-        search.engines = {
-          "Amazon.de".metaData.hidden = true;
-          "bing".metaData.hidden = true;
-          "ebay".metaData.hidden = true;
-          "google".metaData.hidden = true;
-          "wikipedia".metaData.hidden = true;
-        };
-        # about:config
-        settings = {
-          "browser.download.useDownloadDir" = false;
-          "browser.newtabpage.activity-stream.feeds.topsites" = false;
-          "browser.newtabpage.activity-stream.showSearch" = false;
-          "browser.newtabpage.enabled" = false;
-          "browser.search.suggest.enabled" = false;
-          "browser.startup.homepage" = "chrome://browser/content/blanktab.html";
-          "browser.tabs.firefox-view" = false;
-          "browser.toolbars.bookmarks.visibility" = "never";
-          "browser.translations.automaticallyPopup" = false;
-          "browser.urlbar.suggest.addons" = false;
-          "browser.urlbar.suggest.bestmatch" = false;
-          "browser.urlbar.suggest.bookmark" = false;
-          "browser.urlbar.suggest.calculator" = false;
-          "browser.urlbar.suggest.clipboard" = false;
-          "browser.urlbar.suggest.engines" = false;
-          "browser.urlbar.suggest.history" = false;
-          "browser.urlbar.suggest.mdn" = false;
-          "browser.urlbar.suggest.pocket" = false;
-          "browser.urlbar.suggest.remotetabe" = false;
-          "browser.urlbar.suggest.search" = false;
-          "browser.urlbar.suggest.topsites" = false;
-          "browser.urlbar.suggest.trending" = false;
-          "browser.urlbar.suggest.weather" = false;
-          "datareporting.healthreport.uploadEnabled" = false;
-          "extensions.pocket.enabled" = false;
-          "layout.spellcheckDefault" = 0;
-          "privacy.clearOnShutdown.cookies" = true;
-          "privacy.clearOnShutdown.history" = true;
-          "privacy.clearOnShutdown.siteSettings" = true;
-          "privacy.donottrackheader.enabled" = true;
-          "signon.autofillForms" = false;
-          "signon.management.page.breach-alerts.enabled" = false;
-        };
       };
     };
+
+    # about:config — set as default (user can override) or locked
+    preferences = {
+      "browser.download.useDownloadDir" = false;
+      "browser.newtabpage.activity-stream.feeds.topsites" = false;
+      "browser.newtabpage.activity-stream.showSearch" = false;
+      "browser.newtabpage.enabled" = false;
+      "browser.search.suggest.enabled" = false;
+      "browser.startup.homepage" = "chrome://browser/content/blanktab.html";
+      "browser.tabs.firefox-view" = false;
+      "browser.toolbars.bookmarks.visibility" = "never";
+      "browser.translations.automaticallyPopup" = false;
+      "browser.urlbar.suggest.addons" = false;
+      "browser.urlbar.suggest.bestmatch" = false;
+      "browser.urlbar.suggest.bookmark" = false;
+      "browser.urlbar.suggest.calculator" = false;
+      "browser.urlbar.suggest.clipboard" = false;
+      "browser.urlbar.suggest.engines" = false;
+      "browser.urlbar.suggest.history" = false;
+      "browser.urlbar.suggest.mdn" = false;
+      "browser.urlbar.suggest.pocket" = false;
+      "browser.urlbar.suggest.remotetabe" = false;
+      "browser.urlbar.suggest.search" = false;
+      "browser.urlbar.suggest.topsites" = false;
+      "browser.urlbar.suggest.trending" = false;
+      "browser.urlbar.suggest.weather" = false;
+      "datareporting.healthreport.uploadEnabled" = false;
+      "extensions.pocket.enabled" = false;
+      "layout.spellcheckDefault" = 0;
+      "privacy.clearOnShutdown.cookies" = true;
+      "privacy.clearOnShutdown.history" = true;
+      "privacy.clearOnShutdown.siteSettings" = true;
+      "privacy.donottrackheader.enabled" = true;
+      "signon.autofillForms" = false;
+      "signon.management.page.breach-alerts.enabled" = false;
+    };
+
+    preferencesStatus = "default";
   };
 }
